f1c314b213c394801aa6d2c65ed25533eeb7014f
Batjax <34157943+Batjax@users.noreply.github.com>
2018-10-02 16:27:12 +0200

    exploration en mode paysage

--- exploration.php
+++ exploration.php
@@ -6,6 +6,20 @@
     <head>
         <meta charset="utf-8">
         <title>HEP Explore</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <script src="js/pleaserotate.min.js?ver=0.2" type="text/javascript"></script>
+        <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,500" rel="stylesheet">
+
+        <link rel="stylesheet" href="css/bootstrap.css">
+        <link rel="stylesheet" href="css/animate.css">
+        <link rel="stylesheet" href="css/owl.carousel.min.css">
+
+        <link rel="stylesheet" href="fonts/ionicons/css/ionicons.min.css">
+        <link rel="stylesheet" href="fonts/fontawesome/css/font-awesome.min.css">
+        <link rel="stylesheet" href="fonts/flaticon/font/flaticon.css">
+
+        <!-- Theme Style -->
+        <link rel="stylesheet" href="css/style.css">
         <script type="text/javascript" src="js/exploration.js"></script>
     </head>
     <body>
@@ -16,4 +30,6 @@
             $nom = $_SESSION['nom'];
         ?>
     </body>
+    <img style="width:100%; height:100%;" src="images/exploration/leclair---2e-etage.jpg"/>
+    <button type="button">Changer </button>
 </html>
