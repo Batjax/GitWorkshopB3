7e0fa8b09af328e132d384708b50b2845be73ee1
laube <laube@DESKTOP-S7PIDLR>
2018-10-02 15:20:29 +0200

    ...

--- index.php
+++ index.php
@@ -27,20 +27,22 @@
             <div class="row align-items-center">
                 <div class="col-md-12">
                     <div>
-                        <div class="heading">
+                        <div style="text-align: center;" class="heading">
                             <h2>Bienvenu dans HEP Explore</h2>
                         </div>
+                        <br>
                         <div>
-                            <div style="float:center;">
+                            <div style="text-align: center;">
                                 <a href="visite.php" class="btn btn-primary reverse py-2 px-3">Explorez le campus</a>
                             </div>
                             <br>
-                            <p>Dans « Explorez le campus » vous pourrez explorer de manière interactive le campus Renée-Cassin et ses environs accompagné par Hep l’étudiant.</p> 
-                            <div style="float:center">
+                            <p>Ici vous pourrez explorer de manière interactive le campus Renée-Cassin et ses environs accompagné par Hep l’étudiant.</p> 
+                            <br>
+                            <div style="text-align: center;">
                                 <a href="info.html" class="btn btn-primary reverse py-2 px-3">Informations sur le campus</a>
                             </div>                                
                             <br>
-                            <p>Dans « Informations sur le campus » vous trouverez toutes les informations liées au campus HEP René-Cassin : écoles et centres de formation, espace dédié aux incubateurs, événements culturels, conférence et bien d’autres ...</p> 
+                            <p>Là vous trouverez toutes les informations liées au campus HEP René-Cassin : écoles et centres de formation, espace dédié aux incubateurs, événements culturels, conférence et bien d’autres ...</p> 
                         </div>
                     </div>
                 </div>
